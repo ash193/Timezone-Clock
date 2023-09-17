83aadd80f31afdc38a095c540c73b3c693a8bf31
Ashley <ash.193@live.com>
2023-09-16 21:56:18 -0400

    Updates

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);
+
+let njElement = document.querySelector("#new-jersey");
+let njDateElement = njElement.querySelector(".date");
+let njTimeElement = njElement.querySelector(".time");
+let njTime = moment().tz("Europe/Paris");
+
+njElement.innerHTML = njTime.format("MMMM Do YYYY");
+njElement.innerHTML = njTime.format("h:mm:ss [<small>]A[</small>]");
