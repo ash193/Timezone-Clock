4e7d08e95dbd4396a36a4edfbc6be3c142f3db52
Ashley <ash.193@live.com>
2023-09-20 00:02:53 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM     Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format(
   "h:mm:ss [<small>]A[</small>]"
 );
